103f05ab24bba4b64be78194ec9f32ae88ad2d25
Martun Rafayelyan <rafayelyan777@mail.ru>
2023-11-21 23:42:56 +0400

    game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Game from "./components/game";
+import "./components/style.css"
+
+function App() {
+  return (
+      <div className="">
+        <Game />
+      </div>
+  );
+}
+
+export default App;
